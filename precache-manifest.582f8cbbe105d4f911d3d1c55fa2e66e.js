95e7e20f8a590f633f9a078324ff7c9118e07ab3
Taras Rushchak <taraskhust@gmail.com>
2019-06-22 10:54:08 +0300

    Updates

---
+++ precache-manifest.582f8cbbe105d4f911d3d1c55fa2e66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "110cea2e3a16e73561fa",
+    "url": "/static/css/main.cf3b6afe.chunk.css"
+  },
+  {
+    "revision": "110cea2e3a16e73561fa",
+    "url": "/static/js/main.bb9d721b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e368908f3b7fa8765f88",
+    "url": "/static/css/2.d7a93de2.chunk.css"
+  },
+  {
+    "revision": "e368908f3b7fa8765f88",
+    "url": "/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "84cef4926f5c93768b42bc813a5c757a",
+    "url": "/index.html"
+  }
+];
